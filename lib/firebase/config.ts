d41de64904151905237a82bc9ea3038aace70ef7
sahlimouna <135168993+sahlimouna@users.noreply.github.com>
2025-06-14 03:05:54 +0100

    Update config.ts

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -6,10 +6,10 @@ import { getMessaging } from "firebase/messaging"
 import { doc, getDoc, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: AIzaSyCv_yTxIDDIRTX4rWDwv_D2pPgYI33gWKk,
-  authDomain: gari-parkinglocal.firebaseapp.com,
-  projectId: gari-parkinglocal,
-  storageBucket: gari-parkinglocal.firebasestorage.app,
+  apiKey: "AIzaSyCv_yTxIDDIRTX4rWDwv_D2pPgYI33gWKk",
+  authDomain: "gari-parkinglocal.firebaseapp.com",
+  projectId: "gari-parkinglocal",
+  storageBucket: "gari-parkinglocal.firebasestorage.app",
   messagingSenderId: "484158155675",
   appId: "1:484158155675:web:9d69bcb0b10085b2cf0f3e",
 }
